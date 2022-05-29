f5aa7cf5d13a3878a232687da1e8b15ebcaf4fca
Dennis Mahle <dennis.mahle@joyent.com>
2022-05-28 17:47:20 -0700

    drj update3

--- src/timepy.py
+++ src/timepy.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
+from pprint import pprint
 import pytz
-import argparse
 import sys
-
+import argparse
 from datetime import datetime as dt
+
+parser = argparse.ArgumentParser(description='Call time based on timezone')
+
+parser.add_argument('-t',
+                    '--timezones',
+                    action='store_true',
+                    help='List timezones')
+parser.add_argument('-l',
+                    '--location',
+                    default='Vancouver',
+                    help='Specify location[s]')
+
+args = parser.parse_args()
 
 zt = dt.utcnow()
 lt = dt.now()
 
 # Print UTC
-print(f"{zt}Z\t\tUTC")
+print(f"UTC:\t{zt}Z")
 
 # Search for tz on args:
-count = 0
-while count != len(sys.argv) - 1:
-    count += 1
-    for i in pytz.all_timezones:
-        if sys.argv[count] in i:
-            print(f"{sys.argv[count]}:\t{lt.astimezone(pytz.timezone(i))}")
+for i in pytz.all_timezones:
+    if args.timezones:
+        print(f"{i}")
+    elif args.location in i:
+        print(f"{args.location}:\t{lt.astimezone(pytz.timezone(i))}")
