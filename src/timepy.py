0643c66cb5580246dab3cdc928561907649a142f
Dennis Mahle <dennis.mahle@joyent.com>
2022-05-28 17:37:02 -0700

    format (yapf)

--- src/timepy.py
+++ src/timepy.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import pytz
 from datetime import datetime as dt
-tzs = {'Seoul' : 'Asia/Seoul', 'San Francisco' : 'US/Pacific', 'London' : 'Europe/London'}
+
+tzs = {
+    'Seoul': 'Asia/Seoul',
+    'San Francisco': 'US/Pacific',
+    'London': 'Europe/London'
+}
 
 zt = dt.utcnow()
 lt = dt.now()
@@ -12,4 +17,3 @@ print(f"{zt}Z\t\tUTC \n")
 # Print time in dict.
 for a in tzs:
     print(f"{lt.astimezone(pytz.timezone(tzs[a]))}\t{a}")
-
